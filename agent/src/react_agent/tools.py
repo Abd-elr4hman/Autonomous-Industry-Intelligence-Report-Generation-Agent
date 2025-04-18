913b0f89c42dde50b445ef105fa6d0ccdcf8c86f
Abd-elr4hman <ab.hassanein@gmail.com>
2025-04-18 21:47:35 +0200

    Initializing simple langgraph agent

---
+++ agent/src/react_agent/tools.py
@@ -0,0 +1,49 @@
+"""This module provides example tools for web scraping and search functionality.
+
+It includes a basic Tavily search function (as an example)
+
+These tools are intended as free examples to get started. For production use,
+consider implementing more robust and specialized tools tailored to your needs.
+"""
+
+from typing import Any, Callable, List, Optional, cast
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.runnables import RunnableConfig
+from langchain_core.tools import InjectedToolArg
+from typing_extensions import Annotated
+
+from react_agent.configuration import Configuration
+from langchain_core.tools import tool
+from langchain_experimental.utilities import PythonREPL
+
+async def search(
+    query: str, *, config: Annotated[RunnableConfig, InjectedToolArg]
+) -> Optional[list[dict[str, Any]]]:
+    """Search for general web results.
+
+    This function performs a search using the Tavily search engine, which is designed
+    to provide comprehensive, accurate, and trusted results. It's particularly useful
+    for answering questions about current events.
+    """
+    configuration = Configuration.from_runnable_config(config)
+    wrapped = TavilySearchResults(max_results=configuration.max_search_results)
+    result = await wrapped.ainvoke({"query": query})
+    return cast(list[dict[str, Any]], result)
+
+repl = PythonREPL()
+
+@tool
+def python_repl_tool(
+    code: Annotated[str, "The python code to execute to generate your chart."],
+):
+    """Use this to execute python code and do math. If you want to see the output of a value,
+    you should print it out with `print(...)`. This is visible to the user."""
+    try:
+        result = repl.run(code)
+    except BaseException as e:
+        return f"Failed to execute. Error: {repr(e)}"
+    result_str = f"Successfully executed:\n\`\`\`python\n{code}\n\`\`\`\nStdout: {result}"
+    return result_str
+
+TOOLS: List[Callable[..., Any]] = [search]
